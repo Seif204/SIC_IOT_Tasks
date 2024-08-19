e16c6b4f13f48f4b76139122054c65a2b9795858
Seif Ahmed <seas.4.2002@gmail.com>
2024-08-20 00:01:31 +0300

    ADD TEMP AND&DIST PLOT TASK

--- CPU_Monitring_Task/CPU_Moitring_Task.py
+++ CPU_Monitring_Task/CPU_Moitring_Task.py
@@ -2,32 +2,33 @@ from gpiozero import LED
 import psutil
 import time 
 from datetime import datetime
-#green_led = LED(20)
-#yellow_led = LED(21)
-#red_led = LED(22)
+import matplotlib.pyplot as plt
+
+green_led = LED(20)
+yellow_led = LED(21)
+red_led = LED(22)
 #monitring_file = open("monitring_file.txt" , "w")
 #monitring_file.close()
 while True :
     monitring_file = open("monitring_file.txt" , "a")
-
     cpuusages = psutil.cpu_percent(interval=1, percpu=True)
     cpuusagemean = round(sum(cpuusages) / len(cpuusages))
     print(f"CPU usage(%): {cpuusagemean}%")
-    #if cpuusagemean < 50:
-        #green_led.on()  
-        #yellow_led.off()
-        #red_led.off()
-        #print("CPU usage is less than 50%")
-    #elif cpuusagemean >= 50 and cpuusagemean < 80:
-        #green_led.off()
-        #yellow_led.on()  
-        #red_led.off()
-        #print("CPU usage is between 50% and 80%")
-    #else:
-        #green_led.off()
-        #yellow_led.off()
-        #red_led.on()  
-        #print("CPU usage is above or equal to 80%")
+    if cpuusagemean < 50:
+        green_led.on()  
+        yellow_led.off()
+        red_led.off()
+        print("CPU usage is less than 50%")
+    elif cpuusagemean >= 50 and cpuusagemean < 80:
+        green_led.off()
+        yellow_led.on()  
+        red_led.off()
+        print("CPU usage is between 50% and 80%")
+    else:
+        green_led.off()
+        yellow_led.off()
+        red_led.on()  
+        print("CPU usage is above or equal to 80%")
     data =f"{datetime.now().strftime('%Y/%m/%d %HH %MM %SS')}" \
           f"cpu usage(%) : {cpuusagemean}% \n"  
     monitring_file.write(data)
